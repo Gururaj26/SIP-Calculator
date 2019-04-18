7e4c12fa0373cd06113d8020e235c6ab21dff80f
Gururaj Dharani <dharani.gururaj@gmail.com>
2019-04-19 01:49:40 +0530

    initital commit: add values in codebase and print the investment plan on markdown

---
+++ app.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const helpers = require('./helpers');
+const tableHeader = helpers.tableHeader;
+const investmentObj = {
+  currentInvestment: 3000,
+  interestRate: 13.14,
+  investmentTerm: 12,
+}
+
+function calculateSip() {
+  const totalInvestment = investmentObj.currentInvestment * investmentObj.investmentTerm;
+  const interestEarned = Math.floor((totalInvestment * investmentObj.interestRate) / 100);
+  const corpusWithCAGR = totalInvestment + interestEarned;
+  const updatedInvestmentObj = {
+    ...investmentObj,
+    totalInvestment: putCommas(totalInvestment),
+    interestEarned: putCommas(interestEarned),
+    corpusWithCAGR: putCommas(corpusWithCAGR),
+  }
+  writeCalculationToFile(updatedInvestmentObj);
+}
+
+function putCommas(amount){
+  return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function writeCalculationToFile(updatedInvestmentObj){
+  const tableBody = `| ${updatedInvestmentObj.currentInvestment} | ${updatedInvestmentObj.investmentTerm} | ${updatedInvestmentObj.interestRate} | ${updatedInvestmentObj.totalInvestment} | ${updatedInvestmentObj.interestEarned} | ${updatedInvestmentObj.corpusWithCAGR}`
+  const fullTable = tableHeader.concat("\n" + tableBody);
+  fs.writeFile("calculation.md", fullTable, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+      console.log("The file was saved!");
+  });
+}
+
+calculateSip();
---
+++ helpers.js
@@ -0,0 +1,7 @@
+const tableHeader = `| Monthly Investment | Investment Term | Assumed Interest |  Total Amount Invested in the term  | + Gains  |  Corpus With CAGR  |
+| ------------- |:-------------:|:-------------:|:-------------:|:-------------:|---------:|`
+
+
+module.exports = {
+  tableHeader: tableHeader
+}
